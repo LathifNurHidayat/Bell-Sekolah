995bf5a8379a0eb39d6c456b79472fcae17dcf25
Lathif Nur Hidayat <lathifnurhidayat09@gmail.com>
2024-12-14 23:58:27 +0700

    Add

--- BelSekolah/BelSekolahForm/PopUpForm/PopUp Input Lagu/InputLaguForm.Designer.cs
+++ BelSekolah/BelSekolahForm/PopUpForm/PopUp Input Lagu/InputLaguForm.Designer.cs
@@ -28,14 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InputLaguForm));
             panel1 = new Panel();
             label4 = new Label();
-            JamIstirahat2Text = new TextBox();
+            Lagu3Text = new TextBox();
             label3 = new Label();
-            JamIstirahat1Text = new TextBox();
+            Lagu2Text = new TextBox();
             label1 = new Label();
-            AturButton = new Button();
-            IntervalText = new TextBox();
+            SimpanButton = new Button();
+            Lagu1Text = new TextBox();
+            label2 = new Label();
+            Lagu4Text = new TextBox();
+            BrowseLagu1Button = new Button();
+            BrowseLagu2Button = new Button();
+            BrowseLagu3Button = new Button();
+            BrowseLagu4Button = new Button();
             panel1.SuspendLayout();
             SuspendLayout();
             // 
@@ -44,16 +51,22 @@
             panel1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             panel1.BackColor = Color.Gainsboro;
             panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(BrowseLagu4Button);
+            panel1.Controls.Add(BrowseLagu3Button);
+            panel1.Controls.Add(BrowseLagu2Button);
+            panel1.Controls.Add(BrowseLagu1Button);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(Lagu4Text);
             panel1.Controls.Add(label4);
-            panel1.Controls.Add(JamIstirahat2Text);
+            panel1.Controls.Add(Lagu3Text);
             panel1.Controls.Add(label3);
-            panel1.Controls.Add(JamIstirahat1Text);
+            panel1.Controls.Add(Lagu2Text);
             panel1.Controls.Add(label1);
-            panel1.Controls.Add(AturButton);
-            panel1.Controls.Add(IntervalText);
+            panel1.Controls.Add(SimpanButton);
+            panel1.Controls.Add(Lagu1Text);
             panel1.Location = new Point(12, 12);
             panel1.Name = "panel1";
-            panel1.Size = new Size(400, 322);
+            panel1.Size = new Size(400, 358);
             panel1.TabIndex = 174;
             // 
             // label4
@@ -61,86 +74,166 @@
             label4.AutoSize = true;
             label4.BackColor = Color.Transparent;
             label4.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(20, 167);
+            label4.Location = new Point(20, 156);
             label4.Name = "label4";
             label4.Size = new Size(102, 23);
             label4.TabIndex = 174;
             label4.Text = "Lagu Ke - 3 ";
             // 
-            // JamIstirahat2Text
+            // Lagu3Text
             // 
-            JamIstirahat2Text.BorderStyle = BorderStyle.FixedSingle;
-            JamIstirahat2Text.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            JamIstirahat2Text.Location = new Point(175, 162);
-            JamIstirahat2Text.MaxLength = 2;
-            JamIstirahat2Text.Name = "JamIstirahat2Text";
-            JamIstirahat2Text.Size = new Size(102, 30);
-            JamIstirahat2Text.TabIndex = 173;
-            JamIstirahat2Text.TextAlign = HorizontalAlignment.Center;
+            Lagu3Text.BorderStyle = BorderStyle.FixedSingle;
+            Lagu3Text.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Lagu3Text.Location = new Point(20, 182);
+            Lagu3Text.MaxLength = 2;
+            Lagu3Text.Name = "Lagu3Text";
+            Lagu3Text.Size = new Size(257, 30);
+            Lagu3Text.TabIndex = 173;
+            Lagu3Text.TextAlign = HorizontalAlignment.Center;
             // 
             // label3
             // 
             label3.AutoSize = true;
             label3.BackColor = Color.Transparent;
             label3.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            label3.Location = new Point(20, 117);
+            label3.Location = new Point(20, 86);
             label3.Name = "label3";
             label3.Size = new Size(97, 23);
             label3.TabIndex = 172;
             label3.Text = "Lagu Ke - 2";
             // 
-            // JamIstirahat1Text
+            // Lagu2Text
             // 
-            JamIstirahat1Text.BorderStyle = BorderStyle.FixedSingle;
-            JamIstirahat1Text.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            JamIstirahat1Text.Location = new Point(175, 114);
-            JamIstirahat1Text.MaxLength = 2;
-            JamIstirahat1Text.Name = "JamIstirahat1Text";
-            JamIstirahat1Text.Size = new Size(102, 30);
-            JamIstirahat1Text.TabIndex = 171;
-            JamIstirahat1Text.TextAlign = HorizontalAlignment.Center;
+            Lagu2Text.BorderStyle = BorderStyle.FixedSingle;
+            Lagu2Text.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Lagu2Text.Location = new Point(20, 112);
+            Lagu2Text.MaxLength = 2;
+            Lagu2Text.Name = "Lagu2Text";
+            Lagu2Text.Size = new Size(257, 30);
+            Lagu2Text.TabIndex = 171;
+            Lagu2Text.TextAlign = HorizontalAlignment.Center;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
             label1.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(20, 67);
+            label1.Location = new Point(20, 17);
             label1.Name = "label1";
             label1.Size = new Size(97, 23);
             label1.TabIndex = 170;
             label1.Text = "Lagu Ke - 1";
             // 
-            // AturButton
+            // SimpanButton
             // 
-            AturButton.BackColor = Color.DarkGray;
-            AturButton.FlatAppearance.BorderColor = Color.Black;
-            AturButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            AturButton.ForeColor = Color.White;
-            AturButton.Location = new Point(235, 198);
-            AturButton.Name = "AturButton";
-            AturButton.Size = new Size(102, 38);
-            AturButton.TabIndex = 167;
-            AturButton.Text = "Atur";
-            AturButton.UseVisualStyleBackColor = false;
+            SimpanButton.BackColor = Color.DarkGray;
+            SimpanButton.FlatAppearance.BorderColor = Color.Black;
+            SimpanButton.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            SimpanButton.ForeColor = Color.White;
+            SimpanButton.Location = new Point(280, 303);
+            SimpanButton.Name = "SimpanButton";
+            SimpanButton.Size = new Size(102, 38);
+            SimpanButton.TabIndex = 167;
+            SimpanButton.Text = "Simpan";
+            SimpanButton.UseVisualStyleBackColor = false;
             // 
-            // IntervalText
+            // Lagu1Text
             // 
-            IntervalText.BorderStyle = BorderStyle.FixedSingle;
-            IntervalText.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
-            IntervalText.Location = new Point(175, 66);
-            IntervalText.MaxLength = 2;
-            IntervalText.Name = "IntervalText";
-            IntervalText.Size = new Size(102, 30);
-            IntervalText.TabIndex = 169;
-            IntervalText.TextAlign = HorizontalAlignment.Center;
+            Lagu1Text.BorderStyle = BorderStyle.FixedSingle;
+            Lagu1Text.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Lagu1Text.Location = new Point(20, 43);
+            Lagu1Text.MaxLength = 2;
+            Lagu1Text.Name = "Lagu1Text";
+            Lagu1Text.Size = new Size(257, 30);
+            Lagu1Text.TabIndex = 169;
+            Lagu1Text.TextAlign = HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(20, 224);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 23);
+            label2.TabIndex = 176;
+            label2.Text = "Lagu Ke - 4";
+            label2.Click += this.label2_Click;
+            // 
+            // Lagu4Text
+            // 
+            Lagu4Text.BorderStyle = BorderStyle.FixedSingle;
+            Lagu4Text.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Lagu4Text.Location = new Point(20, 250);
+            Lagu4Text.MaxLength = 2;
+            Lagu4Text.Name = "Lagu4Text";
+            Lagu4Text.Size = new Size(257, 30);
+            Lagu4Text.TabIndex = 175;
+            Lagu4Text.TextAlign = HorizontalAlignment.Center;
+            // 
+            // BrowseLagu1Button
+            // 
+            BrowseLagu1Button.BackColor = Color.Gainsboro;
+            BrowseLagu1Button.FlatAppearance.BorderColor = Color.Black;
+            BrowseLagu1Button.FlatStyle = FlatStyle.Flat;
+            BrowseLagu1Button.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            BrowseLagu1Button.Location = new Point(288, 44);
+            BrowseLagu1Button.Name = "BrowseLagu1Button";
+            BrowseLagu1Button.Size = new Size(94, 29);
+            BrowseLagu1Button.TabIndex = 184;
+            BrowseLagu1Button.Tag = "Jam0";
+            BrowseLagu1Button.Text = "Browse";
+            BrowseLagu1Button.UseVisualStyleBackColor = false;
+            // 
+            // BrowseLagu2Button
+            // 
+            BrowseLagu2Button.BackColor = Color.Gainsboro;
+            BrowseLagu2Button.FlatAppearance.BorderColor = Color.Black;
+            BrowseLagu2Button.FlatStyle = FlatStyle.Flat;
+            BrowseLagu2Button.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            BrowseLagu2Button.Location = new Point(288, 113);
+            BrowseLagu2Button.Name = "BrowseLagu2Button";
+            BrowseLagu2Button.Size = new Size(94, 29);
+            BrowseLagu2Button.TabIndex = 185;
+            BrowseLagu2Button.Tag = "Jam0";
+            BrowseLagu2Button.Text = "Browse";
+            BrowseLagu2Button.UseVisualStyleBackColor = false;
+            // 
+            // BrowseLagu3Button
+            // 
+            BrowseLagu3Button.BackColor = Color.Gainsboro;
+            BrowseLagu3Button.FlatAppearance.BorderColor = Color.Black;
+            BrowseLagu3Button.FlatStyle = FlatStyle.Flat;
+            BrowseLagu3Button.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            BrowseLagu3Button.Location = new Point(288, 183);
+            BrowseLagu3Button.Name = "BrowseLagu3Button";
+            BrowseLagu3Button.Size = new Size(94, 29);
+            BrowseLagu3Button.TabIndex = 186;
+            BrowseLagu3Button.Tag = "Jam0";
+            BrowseLagu3Button.Text = "Browse";
+            BrowseLagu3Button.UseVisualStyleBackColor = false;
+            // 
+            // BrowseLagu4Button
+            // 
+            BrowseLagu4Button.BackColor = Color.Gainsboro;
+            BrowseLagu4Button.FlatAppearance.BorderColor = Color.Black;
+            BrowseLagu4Button.FlatStyle = FlatStyle.Flat;
+            BrowseLagu4Button.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            BrowseLagu4Button.Location = new Point(288, 251);
+            BrowseLagu4Button.Name = "BrowseLagu4Button";
+            BrowseLagu4Button.Size = new Size(94, 29);
+            BrowseLagu4Button.TabIndex = 187;
+            BrowseLagu4Button.Tag = "Jam0";
+            BrowseLagu4Button.Text = "Browse";
+            BrowseLagu4Button.UseVisualStyleBackColor = false;
             // 
             // InputLaguForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(424, 346);
+            ClientSize = new Size(424, 382);
             Controls.Add(panel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "InputLaguForm";
             StartPosition = FormStartPosition.CenterParent;
             panel1.ResumeLayout(false);
@@ -152,11 +245,17 @@
 
         private Panel panel1;
         private Label label4;
-        private TextBox JamIstirahat2Text;
+        private TextBox Lagu3Text;
         private Label label3;
-        private TextBox JamIstirahat1Text;
+        private TextBox Lagu2Text;
         private Label label1;
-        private Button AturButton;
-        private TextBox IntervalText;
+        private Button SimpanButton;
+        private TextBox Lagu1Text;
+        private Label label2;
+        private TextBox Lagu4Text;
+        private Button BrowseLagu4Button;
+        private Button BrowseLagu3Button;
+        private Button BrowseLagu2Button;
+        private Button BrowseLagu1Button;
     }
 }
